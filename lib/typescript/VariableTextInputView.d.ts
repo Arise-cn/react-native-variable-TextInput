06d95f207fb0ca814ce446af89d3d8bd1ebe8609
duom青源 <duomqingyuan@duomqingyuandeMacBook-Pro.local>
2023-05-08 17:20:29 +0800

    feat: add lib

--- lib/typescript/VariableTextInputView.d.ts
+++ lib/typescript/VariableTextInputView.d.ts
@@ -1,61 +1,26 @@
-import { StyleProp, TextStyle, ColorValue, NativeSyntheticEvent, TextInputChangeEventData, ImageResolvedAssetSource, TextInputContentSizeChangeEventData, KeyboardTypeOptions } from 'react-native';
+import { StyleProp, TextStyle, ColorValue, NativeSyntheticEvent, TextInputChangeEventData, TextInputContentSizeChangeEventData, KeyboardTypeOptions } from 'react-native';
 import React from 'react';
-export interface IVTTextInputData {
-    nativeEvent: {
-        text: string;
-    };
-}
-export declare enum ITextType {
-    emoji = 1,
-    normal = 0,
-    tagText = 2
-}
-export interface IInserTextAttachmentItem {
-    type: ITextType;
-    text?: string;
-    color?: ColorValue;
-    tag?: '@' | '#';
-    name?: string;
-    id?: string;
-    img?: ImageResolvedAssetSource;
-    emojiTag?: string;
-    emojiUri?: string;
-}
-export interface IOnTagsType {
-    tag: string;
-    keyWord: string;
-}
+import type { IATTextViewBase, IEmojiData, IVTTextInputData, IonMentionData } from './exTypes';
 interface IProps {
-    onMention?: () => void;
     style?: StyleProp<TextStyle> | undefined;
     placeholder?: string;
     placeholderTextColor?: ColorValue;
     keyboardAppearance?: 'default' | 'light' | 'dark';
     onChange?: (e: NativeSyntheticEvent<TextInputChangeEventData>) => void;
     onChangeText?: (text: string) => void;
-    value?: string;
     maxTextLength?: number;
     text?: string;
     blurOnSubmit?: boolean;
     onTextInput?: (event: IVTTextInputData) => void;
-    onAndroidTextInput?: (event: IVTTextInputData) => void;
     onContentSizeChange?: (e: NativeSyntheticEvent<TextInputContentSizeChangeEventData>) => void;
     underlineColorAndroid?: ColorValue;
-    onAndroidChange?: (e: NativeSyntheticEvent<TextInputChangeEventData>) => void;
-    onAndroidContentSizeChange?: (e: NativeSyntheticEvent<TextInputContentSizeChangeEventData>) => void;
     keyboardType?: KeyboardTypeOptions | undefined;
     onSubmitEditing?: (text: string) => void;
-    onAndroidSubmitEditing?: (text: string) => void;
     submitBehavior?: 'submit';
+    emojiData?: IEmojiData[];
+    mentions?: string[];
+    onMention?: (data: IonMentionData) => void;
 }
-export type IATTextViewBase = {
-    focus: () => void;
-    blur: () => void;
-    insertEmoji: (img: IInserTextAttachmentItem) => void;
-    insertMentions: (data: IInserTextAttachmentItem) => void;
-    changeAttributedText: (data: IInserTextAttachmentItem[]) => void;
-    dismissTag: () => void;
-};
 export type IATTextViewRef = React.ForwardedRef<IATTextViewBase>;
 declare const VariableTextInputView: React.ForwardRefExoticComponent<IProps & React.RefAttributes<IATTextViewBase>>;
 export { VariableTextInputView };
--- lib/typescript/index.d.ts
+++ lib/typescript/index.d.ts
@@ -1,2 +1,4 @@
 export * from './VariableTextInputView';
+export * from './Util';
+export * from './exTypes';
 //# sourceMappingURL=index.d.ts.map
